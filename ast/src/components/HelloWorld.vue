b02c230ce0e8a18fa766a1f3d3546043241a5509
liupeixin23 <17835698910@163.com>
2022-04-08 16:01:20 +0800

    feat:demo编写

--- ast/src/components/HelloWorld.vue
+++ ast/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2022-04-07 14:34:56
- * @LastEditTime: 2022-04-07 17:14:23
+ * @LastEditTime: 2022-04-08 10:26:04
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /vue-ast/ast/src/components/HelloWorld.vue
@@ -16,7 +16,7 @@
 </template>
 
 <script>
-import { newCode, oldCode } from './test'
+import { newCode, oldCode } from './demo2'
 import { codemirror } from 'vue-codemirror-lite'
 export default {
   name: 'HelloWorld',
